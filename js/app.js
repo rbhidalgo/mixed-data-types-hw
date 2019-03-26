06c19806157000682e60b3ae2931afe57dc1a213
Rogelio Hidalgo <rhidalgo@Rogelios-MacBook-Pro.local>
2019-03-26 07:38:25 -0700

    object-ception

--- js/app.js
+++ js/app.js
@@ -121,3 +121,28 @@ console.log(crazyObject.larry.quotes[0])
 console.log(crazyObject.larry.characters[2].favourtieHobby)
 console.log(crazyObject.larry.nicknames[1])
 console.log(crazyObject.larry.characters[1].name)
+
+// 4. Object-ception
+
+const inception = {
+    reality: {
+        dreamLayer1: {
+            dreamLayer2: {
+                dreamLayer3: {
+                    dreamLayer4: {
+                        dreamLayer5: {
+                            dreamLayer6: {
+                                limbo: "Joseph Gordon Levitt"
+                           }
+                        }
+                    }
+                }
+            }
+        }
+    }
+ }
+
+//  Change the value of limbo to null.
+
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6)
